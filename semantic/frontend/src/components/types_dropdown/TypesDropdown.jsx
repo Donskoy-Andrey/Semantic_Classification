80f41c67eac5a297f7bdf331beb9408d073664ad
Artem <artiebears@mail.ru>
2024-04-13 01:14:55 +0300

    add styles

--- semantic/frontend/src/components/types_dropdown/TypesDropdown.jsx
+++ semantic/frontend/src/components/types_dropdown/TypesDropdown.jsx
@@ -3,16 +3,24 @@ export const TypesDropdown = (props) => {
     const onChange = props.onChange;
     const currentDocType = props.currentDocType;
     const documentTypes = props.documentTypes;
+
+    const handleChange = (event) => {
+        onChange(event.target.value);
+    }
+
     return (
-        <div className="btn-group">
-            <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                {documentTypes[currentDocType]}
-            </button>
-            <ul className="dropdown-menu">
+        <div className="">
+            <h3>Выберите тип документа:</h3>
+            <select
+                className="form-select form-select-lg mb-3"
+                name={"select"}
+                value={currentDocType}
+                onChange={handleChange}
+            >
                 {Object.keys(documentTypes).map(key => (
-                    <li key={key}><p className="dropdown-item" onClick={() => onChange(key)}>{documentTypes[key]}</p></li>
+                    <option key={key} value={key}>{documentTypes[key]}</option>
                 ))}
-            </ul>
+            </select>
         </div>
     )
 }
