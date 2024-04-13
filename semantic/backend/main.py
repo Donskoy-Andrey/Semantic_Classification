25c2d5468e39ca6d526d2f6a150bb32483518501
Andrey Donskoy <donskoi.com@gmail.com>
2024-04-13 15:19:58 +0300

    refactoring

--- semantic/backend/main.py
+++ semantic/backend/main.py
@@ -1,19 +1,15 @@
 import json
 import os
 import pandas as pd
-
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-import asyncio
-
 from .model import SemanticModel
 
 model_ = SemanticModel()
 
 app = FastAPI()
 
-# Allow requests from localhost:3000
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["localhost", "*"],
@@ -56,6 +52,7 @@ async def upload_files(files: list[UploadFile] = File(...), doctype: str = Form(
     except Exception as e:
         print(e)
         return JSONResponse(content={"message": "Failed to upload files", "error": str(e)}, status_code=500)
+
 
 @app.post("/handle_example")
 async def handle_example(request: dict):
